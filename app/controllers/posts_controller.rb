6c06aa2e550cc198c1a3ce7e21a657483b0312cf
CK <thecurrencykid@gmail.com>
2016-09-06 12:04:23 -0700

    devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if    @post.save
-      redirect_to :root_url
+      redirect_to :root
     else
       render :new
     end
